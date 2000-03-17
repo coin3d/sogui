768367ce209b3efb631323bdb5b78036fc617216
Lars J. Aas <larsa@sim.no>
2000-03-17 13:43:40 +0000

    Some popup menu build code.

--- SoAnyFullViewer.h
+++ SoAnyFullViewer.h
@@ -22,10 +22,58 @@
 #ifndef SOANY_FULLVIEWER_H
 #define SOANY_FULLVIEWER_H
 
+class SoAnyPopupMenu;
+
+enum SoViewerPopupMenuItem {
+  ROOT_MENU = 0,
+  MENUTITLE_ITEM,
+
+  FUNCTIONS_MENU,
+  HELP_ITEM,
+  HOME_ITEM,
+  SET_HOME_ITEM,
+  VIEW_ALL_ITEM,
+  SEEK_ITEM,
+  COPY_VIEW_ITEM,
+  PASTE_VIEW_ITEM,
+
+  DRAWSTYLES_MENU,
+  AS_IS_ITEM,
+  HIDDEN_LINE_ITEM,
+  NO_TEXTURE_ITEM,
+  LOW_RESOLUTION_ITEM,
+  WIREFRAME_ITEM,
+  POINTS_ITEM,
+  BOUNDING_BOX_ITEM,
+  MOVE_SAME_AS_STILL_ITEM,
+  MOVE_NO_TEXTURE_ITEM,
+  MOVE_LOW_RES_ITEM,
+  MOVE_WIREFRAME_ITEM,
+  MOVE_LOW_RES_WIREFRAME_ITEM,
+  MOVE_POINTS_ITEM,
+  MOVE_LOW_RES_POINTS_ITEM,
+  MOVE_BOUNDING_BOX_ITEM,
+  SINGLE_BUFFER_ITEM,
+  DOUBLE_BUFFER_ITEM,
+  INTERACTIVE_BUFFER_ITEM,
+
+  EXAMINING_ITEM,
+  DECORATION_ITEM,
+  HEADLIGHT_ITEM,
+  PREFERENCES_ITEM
+}; // enum SoViewerPopupMenuItem
+
+enum SoViewerPopupMenuRadioGroup {
+  STILL_GROUP = 1,
+  MOVE_GROUP,
+  BUFFER_GROUP
+}; // enum SoViewerPopupMenuRadioGroup
+
 // ************************************************************************
 
 class SoAnyFullViewer {
 public:
+  static SoAnyPopupMenu * buildStandardPopupMenu(void);
 
 }; // class SoAnyFullViewer
 
